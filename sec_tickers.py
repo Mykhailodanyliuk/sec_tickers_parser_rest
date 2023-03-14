e7fdc5a798f9997f13aaf67f952a386129e17493
Mykhailo Danyliuk <mihailodaniliuk@gmail.com>
2023-03-14 17:34:18 +0200

    edited time.sleep

--- sec_tickers.py
+++ sec_tickers.py
@@ -30,6 +30,4 @@ if __name__ == '__main__':
         start_time = time.time()
         upload_sec_tickers_data(url)
         work_time = int(time.time() - start_time)
-        print(work_time)
-        print(14400 - work_time)
-        time.sleep(14400 - work_time)
+        time.sleep(abs(work_time % 14400 - 14400))
